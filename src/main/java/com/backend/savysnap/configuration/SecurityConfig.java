3ee818f328d51018c49ce7bc80e70f6d0a52fd35
Thai Phan Thanh Sang <sang.thaiphan.cit22@eiu.edu.vn>
2026-04-12 00:13:32 +0700

    updated

--- src/main/java/com/backend/savysnap/configuration/SecurityConfig.java
+++ src/main/java/com/backend/savysnap/configuration/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://*.vercel.app"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
